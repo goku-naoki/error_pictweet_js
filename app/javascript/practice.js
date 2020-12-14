934d671d4da89f4aaf213c4f204fe51526a02aeb
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-12-14 17:09:41 +0900

    error

--- app/javascript/practice.js
+++ app/javascript/practice.js
@@ -1,73 +1,58 @@
 $(function(){
 
-  //1 要素取得
-  let dom=$('#textChange')   
+//投稿する対するクリックイベントを実行しましょう
 
-//2 対象要素にイベント設置
+    //1 要素取得
+      let dom=$('#text')   
 
-  dom.on('click',function(){
+    //2 対象要素にイベント設置
 
-    
-//3 イベント発生時の処理を記述
-    dom.text('Ajax投稿する')
-  })
+      dom.on('click',function(){
 
-
-
-
+        
+    //3 イベント発生時の処理を記述
+        dom.text('Ajax投稿する')
+      })
 
 
 
 
 
 
+//SENDを押した際にサブミットイベントを実行しましょう
+
+    //1 要素取得
+      let form=$('#Form')   
 
 
+    //2 対象要素にイベント設置
+
+      form.on('submit',function(e){
+
+        
+    //3 イベント発生時の処理を記述
+        e.preventDefault()
+        let elements=$('#Btn') 
+
+        element.css("background-color","pink")
 
 
+    //発展変
+    // let formData=new FormData(this);
 
+    //     $.ajax({
+    //       url: "/tweet",   //リクエスト先のURL
+    //       type: "POST",     //HTTPメソッド
+    //       data: formData,   //リクエストとして送るデータ。rails側ではparamsとして扱われる
+    //       dataType: 'json', 
+    //       processData: false,
+    //       contentType: false
+    //     })
+    //     .done(function(response){
+    //       alert(response.text)
+    //     })
 
-
-
-
-
-
-
-
-
-
-//1 要素取得
-  let form=$('#Form')   
-
-
-//2 対象要素にイベント設置
-
-  form.on('submit',function(e){
-
-    
-//3 イベント発生時の処理を記述
-    e.preventDefault()
-    let element=$('#Btn') 
-
-    element.css("background-color","pink")
-
-
-//発展変
-// let formData=new FormData(this);
-
-//     $.ajax({
-//       url: "/tweets",   //リクエスト先のURL
-//       type: "POST",     //HTTPメソッド
-//       data: formData,   //リクエストとして送るデータ。rails側ではparamsとして扱われる
-//       dataType: 'json', 
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(response){
-//       alert(response.text)
-//     })
-
- })
+    })
  
 
 })
