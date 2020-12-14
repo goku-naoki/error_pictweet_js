982b53d504fe5323af1b10f35704b1ce10a5d2b1
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-12-14 16:48:14 +0900

    Update practice.js

--- app/javascript/practice.js
+++ app/javascript/practice.js
@@ -1,30 +1,38 @@
 $(function(){
 
 //1 要素取得
-  let element=$('#Btn') 
-  let form=$('#Form')   
-//2 対象要素にイベント設置
- form.on('submit',function(e){
 
+  let form=$('#Form')   
+
+
+//2 対象要素にイベント設置
+
+  form.on('submit',function(e){
+
+    
 //3 イベント発生時の処理を記述
+
+    alert('click')
     e.preventDefault()
+    let element=$('#Btn') 
+
     element.css("background-color","pink")
 
 
 //発展変
-let formData=new FormData(this);
+// let formData=new FormData(this);
 
-    $.ajax({
-      url: "/tweets",   //リクエスト先のURL
-      type: "POST",     //HTTPメソッド
-      data: formData,   //リクエストとして送るデータ。rails側ではparamsとして扱われる
-      dataType: 'json', 
-      processData: false,
-      contentType: false
-    })
-    .done(function(response){
-      alert(response.text)
-    })
+//     $.ajax({
+//       url: "/tweets",   //リクエスト先のURL
+//       type: "POST",     //HTTPメソッド
+//       data: formData,   //リクエストとして送るデータ。rails側ではparamsとして扱われる
+//       dataType: 'json', 
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(response){
+//       alert(response.text)
+//     })
 
  })
  
